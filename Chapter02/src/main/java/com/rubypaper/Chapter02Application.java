839a0e8629d0c0f2939d588f7fbcea753a2f3b23
Daseul Song <ektmf363@naver.com>
2023-09-08 12:35:34 +0900

    second commit

--- Chapter02/src/main/java/com/rubypaper/Chapter02Application.java
+++ Chapter02/src/main/java/com/rubypaper/Chapter02Application.java
@@ -8,6 +8,7 @@ public class Chapter02Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Chapter02Application.class, args);
+		System.out.println("test_git");
 	}
 
 }
